7a60f032137cb0506e42ce61331fe42a79d4acf2
Artem <1ttem12@gmail.com>
2017-02-09 17:55:10 +0100

    Added Person

---
+++ src/main/java/com/ttem/model/account/Person.java
@@ -0,0 +1,20 @@
+package com.ttem.model.account;
+
+public class Person {
+
+    private final String firstName;
+    private final String secondName;
+
+    public Person(final String firstName, final String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+}
